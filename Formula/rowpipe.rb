2ebcbc13005a4234a0debb9926fa89d920701ff9
crafttcore <crafttcore@gmail.com>
2026-09-24 19:15:21 +0300

    chore(ci): remove darwin-x64 target from release pipeline

--- Formula/rowpipe.rb
+++ Formula/rowpipe.rb
@@ -1,21 +1,13 @@
 class Rowpipe < Formula
   desc "Stream-first tabular data toolkit for CSV, TSV, JSON, XLSX, Parquet, and Markdown"
   homepage "https://github.com/litepacks/rowpipe"
-  version "2.10.0"
+  version "2.10.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "#{homepage}/releases/download/v#{version}/rowpipe-darwin-arm64.tar.gz"
       sha256 "REPLACE_WITH_DARWIN_ARM64_SHA256"
-
-      def install
-        bin.install "rowpipe"
-      end
-    end
-    if Hardware::CPU.intel?
-      url "#{homepage}/releases/download/v#{version}/rowpipe-darwin-x64.tar.gz"
-      sha256 "REPLACE_WITH_DARWIN_X64_SHA256"
 
       def install
         bin.install "rowpipe"
