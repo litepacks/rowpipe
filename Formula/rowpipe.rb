dc0dcdf0ebd51530306ad51cf3ee09895fa97416
crafttcore <crafttcore@gmail.com>
2026-09-24 21:53:39 +0300

    chore(formula): update formula checksums for v2.10.4

--- Formula/rowpipe.rb
+++ Formula/rowpipe.rb
@@ -7,7 +7,7 @@ class Rowpipe < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "#{homepage}/releases/download/v#{version}/rowpipe-darwin-arm64.tar.gz"
-      sha256 "REPLACE_WITH_DARWIN_ARM64_SHA256"
+      sha256 "4cb114040b4595148f7bd520c82dcc3b7d97dcd8c024d8fd0d9accc10364e6ce"
 
       def install
         bin.install "rowpipe"
@@ -18,7 +18,7 @@ class Rowpipe < Formula
   on_linux do
     if Hardware::CPU.intel?
       url "#{homepage}/releases/download/v#{version}/rowpipe-linux-x64.tar.gz"
-      sha256 "REPLACE_WITH_LINUX_X64_SHA256"
+      sha256 "695538ef3ef82e687c3d193b9a8f9b44cbc7640f4686c011d1011cb6fd221ac9"
 
       def install
         bin.install "rowpipe"
